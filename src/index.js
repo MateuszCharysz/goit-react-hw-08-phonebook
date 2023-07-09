f06ffb38cb2c2fddaa7e6f83bb2cec503daa83bf
MateuszCharysz <charyszek@interia.pl>
2023-07-09 20:06:27 +0200

    little update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import {BrowserRouter} from 'react-router-dom';
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
+import 'modern-normalize'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
