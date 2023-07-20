af74ba850fdcfb364dc4b6bf3c60bf7f86516332
MateuszCharysz <charyszek@interia.pl>
2023-07-20 21:13:55 +0200

    work on persistor (to local storage)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { contactsReducer } from './contacts/contactsSlice';
 import { filterReducer } from './contacts/filterSlice';
 import { authReducer } from './auth/authslice';
 import { sandboxReducer } from './sandbox/sandboxSlice';
+import {persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const middleware = [...getDef]
+
 
 export const store = configureStore({
   reducer: {
